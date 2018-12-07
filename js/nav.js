bdc45b45529561f6984366e4b80798823976c3f2
YukiBunny <43750630+YukiBunny@users.noreply.github.com>
2018-12-06 22:41:17 -0300

    nav js ready

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,13 @@
 $(function () {
-    $("#boton_nav").click(function () {
-        $("#colapsable").slideToggle(500);
-    })
-})
+    $('#boton_nav').click(function () {
+        $('#colapsable').slideToggle(500);
+    });
+    $('#colapsable_nav').mouseleave(function () {
+        if ($('#colapsable').css('display') != 'none') {
+            $('#colapsable').slideToggle(300);
+        }
+    });
+    $('#colapsable>a').click(function () {
+        $('#colapsable').slideToggle(300);
+    });
+});
