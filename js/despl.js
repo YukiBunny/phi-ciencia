898d0d907cec2ee3e574ab03aa016376e775ff04
thiszantedeschia <sl.castroj@gmail.com>
2018-12-08 23:10:23 -0300

    Added 'nosotros' height animation

--- js/despl.js
+++ js/despl.js
@@ -1,14 +1,19 @@
 $(function () {
-    $('.servicios_despl>h3').click(function () {
-        $(this).siblings('p').slideToggle(500);
-        $(this).children('i').toggleClass('fa-caret-down');
-        $(this).children('i').toggleClass('fa-caret-up')
-    });
-    $('#nosotros_despl>h2').click(function () {
-        var h = $('#nosotros_despl').height();
-        $('#nosotros_box').height(h);
-        $(this).siblings('div').slideToggle(500);
-        $(this).children('i').toggleClass('fa-caret-down');
-        $(this).children('i').toggleClass('fa-caret-up');
-    });
+	$('.servicios_despl>h3').click(function () {
+		$(this).siblings('p').slideToggle(500);
+		$(this).children('i').toggleClass('fa-caret-down');
+		$(this).children('i').toggleClass('fa-caret-up')
+	});
+	$('#nosotros_despl>h2').click(function () {
+		var div = $(this).siblings('div');
+		if (div.css('display') == 'none') {
+			$('#nosotros_box').animate({ 'min-height': div.height() + $(this).outerHeight(true) }, 500);
+		}
+		else {
+			$('#nosotros_box').animate({ 'min-height': '50vh' }, 500);
+		}
+		div.slideToggle(500);
+		$(this).children('i').toggleClass('fa-caret-down');
+		$(this).children('i').toggleClass('fa-caret-up');
+	});
 });
