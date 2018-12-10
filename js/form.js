1f88e0f673c76cdc432e2fb1ce0a407da6a377ea
thiszantedeschia <this.zantedeschia@gmail.com>
2018-12-09 23:45:53 -0300

    Reverted form mailto to open at tab

--- js/form.js
+++ js/form.js
@@ -4,12 +4,12 @@ $(function () {
 		var subject = "?subject=" + $("#form_subject").val();
 		var body = '&body=' + $('#form_message').val().replace(/\n/g, "%0D%0A") + '%0D%0A' + $('#form_ocupation').val();
 
-		location.href = 'mailto:' + to + subject + body;
-
+		window.open('mailto:' + to + subject + body, '_blank');
 	});
-	function formSubmit() {
-		$('#form_subject').val('');
-		$('#form_message').val('');
-		$('#form_ocupation').val('0');
-	}
 });
+
+function formSubmit() {
+	$('#form_subject').val('');
+	$('#form_message').val('');
+	$('#form_ocupation').val('0');
+}
